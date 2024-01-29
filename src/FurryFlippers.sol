5d483d1ec7aa48bb2ae7da7b14b6bc9b6ef4a92e
nemusona <nemusonaneko@protonmail.com>
2024-01-29 17:56:04 -0600

    pyth integration

--- src/FurryFlippers.sol
+++ src/FurryFlippers.sol
@@ -3,13 +3,23 @@ pragma solidity ^0.8.0;
 
 import {Ownable} from "solady/src/auth/Ownable.sol";
 import {SafeTransferLib} from "solady/src/utils/SafeTransferLib.sol";
+import {IEntropy} from "./interfaces/IEntropy.sol";
 
 contract FurryFlippers is Ownable {
     address public token;
     address public lottery;
+    IEntropy public entropy;
+    address public entropyProvider;
     uint256 public fee;
     uint256 public betLength;
-    mapping(bytes32 => uint256) public active;
+
+    mapping(bytes32 => betInformation) public bets;
+
+    struct betInformation {
+        uint96 active;
+        uint96 locked;
+        uint64 sequenceNumber;
+    }
 
     event BetCreated(
         address maker,
@@ -32,6 +42,16 @@ contract FurryFlippers is Ownable {
         address taker,
         uint256 amount,
         bool makerHeads,
+        bytes32 betId
+    );
+
+    event BetSettled(
+        address maker,
+        address taker,
+        uint256 amount,
+        bool makerHeads,
+        bytes32 userRandom,
+        bytes32 providerRandom,
         address winner,
         uint256[] results,
         uint256 lotteryFee,
@@ -42,11 +62,15 @@ contract FurryFlippers is Ownable {
     constructor(
         address _token,
         address _lottery,
+        address _entropy,
+        address _entropyProvider,
         uint256 _fee,
         uint256 _betLength
     ) {
         token = _token;
         lottery = _lottery;
+        entropy = IEntropy(_entropy);
+        entropyProvider = _entropyProvider;
         fee = _fee;
         betLength = _betLength;
     }
@@ -58,6 +82,10 @@ contract FurryFlippers is Ownable {
         bool makerHeads
     ) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(maker, taker, makerHeads, amount));
+    }
+
+    function getEntropyFee() public view returns (uint256) {
+        return entropy.getFee(entropyProvider);
     }
 
     function createBet(
@@ -78,11 +106,18 @@ contract FurryFlippers is Ownable {
         emit BetCancelled(msg.sender, taker, amount, makerHeads, betId);
     }
 
-    function takeBet(
+    function takeBet(address maker, uint256 amount, bool makerHeads) external {
+        bytes32 betId = _takeBet(maker, amount, makerHeads);
+        emit BetTaken(maker, msg.sender, amount, makerHeads, betId);
+    }
+
+    function settleBet(
         address maker,
         address taker,
         uint256 amount,
-        bool makerHeads
+        bool makerHeads,
+        bytes32 userRandom,
+        bytes32 providerRandom
     ) external {
         (
             bytes32 betId,
@@ -90,12 +125,21 @@ contract FurryFlippers is Ownable {
             uint256[] memory results,
             uint256 lotteryFee,
             uint256 payout
-        ) = _takeBet(maker, taker, amount, makerHeads);
-        emit BetTaken(
+        ) = _settleBet(
+                maker,
+                taker,
+                amount,
+                makerHeads,
+                userRandom,
+                providerRandom
+            );
+        emit BetSettled(
             maker,
-            msg.sender,
+            taker,
             amount,
             makerHeads,
+            userRandom,
+            providerRandom,
             winner,
             results,
             lotteryFee,
@@ -104,12 +148,42 @@ contract FurryFlippers is Ownable {
         );
     }
 
-    function changeLotteryAddress(address newLottery) external onlyOwner {
-        lottery = newLottery;
+    function requestSeed(
+        address maker,
+        address taker,
+        uint256 amount,
+        bool makerHeads,
+        bytes32 commitment
+    ) external payable {
+        bytes32 betId = getBetId(maker, taker, amount, makerHeads);
+        betInformation storage bet = bets[betId];
+        require(bet.active == 1, "not active");
+        require(bet.locked == 1, "not locked");
+        require(bet.sequenceNumber != 0, "number already requested");
+        uint256 entropyFee = entropy.getFee(entropyProvider);
+        require(msg.value > entropyFee, "insufficient fee");
+        uint64 sequenceNumber = entropy.request{value: entropyFee}(
+            entropyProvider,
+            commitment,
+            true
+        );
+        bets[betId].sequenceNumber = sequenceNumber;
     }
 
-    function changeLotteryFee(uint256 newFee) external onlyOwner {
-        fee = newFee;
+    function changeParams(
+        address _token,
+        address _lottery,
+        address _entropy,
+        address _entropyProvider,
+        uint256 _fee,
+        uint256 _betLength
+    ) external onlyOwner {
+        token = _token;
+        lottery = _lottery;
+        entropy = IEntropy(_entropy);
+        entropyProvider = _entropyProvider;
+        fee = _fee;
+        betLength = _betLength;
     }
 
     function _createBet(
@@ -118,8 +192,8 @@ contract FurryFlippers is Ownable {
         bool makerHeads
     ) internal returns (bytes32 betId) {
         betId = getBetId(msg.sender, taker, amount, makerHeads);
-        require(active[betId] == 0, "exists");
-        active[betId] = 1;
+        require(bets[betId].active == 0, "already active");
+        bets[betId].active = 1;
         SafeTransferLib.safeTransferFrom(
             token,
             msg.sender,
@@ -134,16 +208,38 @@ contract FurryFlippers is Ownable {
         bool makerHeads
     ) internal returns (bytes32 betId) {
         betId = getBetId(msg.sender, taker, amount, makerHeads);
-        require(active[betId] == 1, "does not exist");
-        active[betId] = 0;
+        betInformation storage bet = bets[betId];
+        require(bet.active == 1, "not active");
+        require(bet.locked == 0, "already locked");
+        bets[betId].active = 0;
         SafeTransferLib.safeTransfer(token, msg.sender, amount);
     }
 
     function _takeBet(
         address maker,
-        address taker,
         uint256 amount,
         bool makerHeads
+    ) internal returns (bytes32 betId) {
+        betId = getBetId(maker, msg.sender, amount, makerHeads);
+        betInformation storage bet = bets[betId];
+        require(bet.active == 1, "not active");
+        require(bet.locked == 0, "already locked");
+        bets[betId].locked = 1;
+        SafeTransferLib.safeTransferFrom(
+            token,
+            msg.sender,
+            address(this),
+            amount
+        );
+    }
+
+    function _settleBet(
+        address maker,
+        address taker,
+        uint256 amount,
+        bool makerHeads,
+        bytes32 userRandom,
+        bytes32 providerRandom
     )
         internal
         returns (
@@ -154,26 +250,27 @@ contract FurryFlippers is Ownable {
             uint256 payout
         )
     {
-        if (taker == address(0)) {
-            betId = getBetId(maker, taker, amount, makerHeads);
-        } else {
-            betId = getBetId(maker, msg.sender, amount, makerHeads);
-        }
-        require(active[betId] == 1, "does not exist");
+        betId = getBetId(maker, taker, amount, makerHeads);
+        require(bets[betId].sequenceNumber != 0, "number not requested");
         uint256 seed = uint256(
-            keccak256(abi.encodePacked(block.number, block.prevrandao))
+            entropy.reveal(
+                entropyProvider,
+                bets[betId].sequenceNumber,
+                userRandom,
+                providerRandom
+            )
         );
-        uint256 makerScore = 0;
-        uint256 takerScore = 0;
-        uint256 i = 0;
+        uint256 makerScore;
+        uint256 takerScore;
+        uint256 i;
         uint256 convertedSeed;
         results = new uint256[](betLength);
         while (i < betLength) {
             seed = uint256(keccak256(abi.encode(seed)));
             unchecked {
-                convertedSeed = seed % 100;
+                convertedSeed = seed % 2;
                 results[i] = convertedSeed;
-                if (convertedSeed < 50) {
+                if (convertedSeed == 0) {
                     ++makerScore;
                 } else {
                     ++takerScore;
@@ -181,23 +278,17 @@ contract FurryFlippers is Ownable {
                 ++i;
             }
         }
-        active[betId] = 0;
         if (makerScore > takerScore) {
             winner = maker;
         } else {
             winner = msg.sender;
         }
-        payout = amount * 2;
         unchecked {
+            payout = amount * 2;
             lotteryFee = (payout * fee) / 10000;
             payout = payout - lotteryFee;
         }
-        SafeTransferLib.safeTransferFrom(
-            token,
-            msg.sender,
-            address(this),
-            amount
-        );
+        delete bets[betId];
         SafeTransferLib.safeTransfer(token, lottery, lotteryFee);
         SafeTransferLib.safeTransfer(token, winner, payout);
     }
